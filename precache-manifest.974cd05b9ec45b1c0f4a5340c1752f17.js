61ec90a78a07abb3e775dc0ef7d32a6bfc2484e1
danielo515 <rdanielo@gmail.com>
2018-11-28 07:44:22 +0100

    Updates

---
+++ precache-manifest.974cd05b9ec45b1c0f4a5340c1752f17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "6dec7d9f10a568c4094f1714be356ae5",
+    "url": "./static/media/report.6dec7d9f.png"
+  },
+  {
+    "revision": "33ba4ece1d77a8baff9dd97d6b491b97",
+    "url": "./static/media/placeholder.33ba4ece.svg"
+  },
+  {
+    "revision": "b1ca65bdd6bd18b3288b7f4f129f6253",
+    "url": "./static/media/dead.b1ca65bd.svg"
+  },
+  {
+    "revision": "4b2b5999eb07bf206fee2af6ec467c02",
+    "url": "./static/media/dead.4b2b5999.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6915acc8f6a564566ab3",
+    "url": "./static/js/main.6915acc8.chunk.js"
+  },
+  {
+    "revision": "84033bb5bfd7c3da30b7",
+    "url": "./static/js/1.84033bb5.chunk.js"
+  },
+  {
+    "revision": "6915acc8f6a564566ab3",
+    "url": "./static/css/main.8d90be0f.chunk.css"
+  },
+  {
+    "revision": "085878f73553a51e763c75f8fa31854f",
+    "url": "./index.html"
+  }
+];
