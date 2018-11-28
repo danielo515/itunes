03078ea6d2571829105f13f6e8f0d5bf455d9141
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2018-11-28 04:53:36 +0100

    feat: boilerplate

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import App from "./containers/App";
+import "./styles.css";
+import * as serviceWorker from './serviceWorker';
+
+
+const target = document.getElementById('root');
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  target
+);
+process.env.NODE_ENV === 'production' ? serviceWorker.register() : serviceWorker.unregister();
